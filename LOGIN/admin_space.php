ce5c91f475e1eccc79894f7e728a19f1fd3f72d6
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-01-06 23:01:41 +0100

    Update admin_space.php

--- LOGIN/admin_space.php
+++ LOGIN/admin_space.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['user']))
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="StyleAdmin.css">
+    <link rel="stylesheet" href="StyleA.css">
     <style>
 
       .card a{
