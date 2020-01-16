aed933a81dee57f69b85c894000b5efe245a6923
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-01-16 01:48:50 +0100

    Update admin_space.php

--- LOGIN/admin_space.php
+++ LOGIN/admin_space.php
@@ -17,7 +17,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="StyleAdmin.css">
+    <link rel="stylesheet" href="admin.css">
     <style>
 
       .card a{
@@ -39,26 +39,54 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
     
 </head>
 <body>
+  <!-- Modal de salle -->
+  <div class="modal fade" id="details" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalScrollableTitle">Ajouter une salle</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form action="../PHP/AjoutSalle.php" method="post">
+            <div>
+                <label>Nom de la salle :
+                <input type="text" name="Ajout_salle" placeholder="Nom de la salle"></label>
+            </div>
+            <button type="submit" class="btn btn-primary">Ajouter</button>
+            </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 
-<!---->
+<!-- espace admin = tableau -->
 <main>
 <div class="container my-5">
        <div class="card-body text-center">
     <h2 class="card-title">Espace réservé à l'administrateur de l'Université!</h2>
   </div>
     <div class="card">
-        <button id="add_new_list" type="button" class="btn btn-primary position-absolute" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-plus"></i> <a href="../Form/index.php"> Ajouter un nouveau évenement</a></button>
+        <button id="add_new_list" type="button" class="btn btn-primary position-absolute" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fas fa-plus"></i> <a href="../Form/index.php"> Ajouter un nouveau évenement </a></button>
+        
         
         <table class="table table-hover">
             <thead>
               <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name Event</th>
-                <th scope="col">théme</th>
+                <!-- <th scope="col">ID</th> -->
+                <th scope="col">Nom d'événement</th>
+                <th scope="col">Théme</th>
                 <th scope="col">Date début</th>
                 <th scope="col">Date fin</th>
-                <th scope="col">Salle</th>
                 <th scope="col">Action</th>
+                <th><button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#details">
+                <i class="fas fa-trash-alt"></i> Ajouter salle </button></th>
               </tr>
             </thead>
             <tbody>
@@ -70,19 +98,34 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
               ?>
               
               <tr>
-                <th scope="row"><?php echo $donnees['id_ev']?></th>
+                <!-- <th scope="row"><?php echo $donnees['id_ev']?></th> -->
                 <td><?php echo  $donnees['intitulé']?></td>
                 <td><?php  echo $donnees['thème']?></td>
                 <td><?php echo $donnees['date_db']; ?></td>
                 <td><?php echo $donnees['date_fn']; ?></td>
-                <td><?php echo $donnees['salle']?></td>
+                <?php
+                $dateDeb = $donnees['date_db'];
+                $datefn = $donnees['date_fn'];
+                $idSalle = $donnees['id_salle'];
+                $req = $bdd->query("SELECT * FROM calendar WHERE date_db='".$dateDeb."' AND date_fn='".$datefn."' AND id_salle='".$idSalle."'");
+
+                if(!($donn = $req->fetch()))
+                {
+
+                ?>
                 <td>
-                    <a class="btn btn-sm btn-primary" href="../Form/index.php"><i class="far fa-edit"></i> Modifier</a>
-                    <a class="btn btn-sm btn-success" href="../PHP/publier.php"><i class="fas fa-trash-alt"></i>Publier</a>
-                    <a class="btn btn-sm btn-danger" href="../PHP/detail.php"><i class="fas fa-trash-alt"></i>Détails</a> 
+                    <a class="btn btn-sm btn-primary" href="../PHP/modifier.php?id=<?php echo($donnees['id_ev']); ?>"><i class="far fa-edit"></i> Modifier</a>
+                    <a class="btn btn-sm btn-success" href="../PHP/publier.php?id=<?php echo($donnees['id_ev']); ?>"><i class="fas fa-trash-alt"></i>Publier</a>
+                     <!-- <a class="btn btn-sm btn-danger" href="../PHP/modal.php"><i class="fas fa-trash-alt"></i>Détails</a>  -->
+                    
+                    <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#detail">
+                    Détails
+                    </button>
                            
                 </td>
-                
+                <?php
+                }
+                ?>
               </tr>
               <?php
               }
@@ -131,12 +174,16 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
 </div>
 </div>
 </main>
-<!---->
+<!--  -->
 <center>
 <form action="LogOut.php" method='post'>
     <button type='submit' value='Log out' class='btn btn-primary'>Se déconnecter</button>
 </form>
 </center>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body> 
 </html>
 
