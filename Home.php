733f7400f91dbcafbf9f6526443b73bfff84e6e0
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-01-16 18:35:45 +0100

    Update Home.php

--- Home.php
+++ Home.php
@@ -17,53 +17,15 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
     
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
 
-   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="Style2.css">
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
-   <script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
    
-  $(document).ready(function() {
-   var calendar = $('#calendar').fullCalendar({
-    editable: false,
-    header:{
-     left:'prev,next today',
-     center:'title',
-     right:'month,agendaWeek,agendaDay'
-    },
-    <?php $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''); ?>
-    events:[ <?php $reponse = $bdd->query('SELECT * FROM calendar'); 
-                  while ($donnees = $reponse->fetch())
-    { ?>
-        {
-            'title':"<?php echo  $donnees['intitulé']?>",
-            'start':'<?php echo $donnees['date_db']; ?>',
-            'end':'<?php echo $donnees['date_fn']; ?>'
-        },
-    
-<?php } ?>],
-    eventClick:function(event)
-    {
-     if(confirm("Are you sure you want to remove it?"))
-     {
-      var id = event.id;
-      $.ajax({
-       url:"../Calendar/delete.php",
-       type:"POST",
-       data:{id:id},
-       success:function()
-       {
-        calendar.fullCalendar('refetchEvents');
-        alert("Event Removed");
-       }
-      })
-     }
-    },
-   });
-  });
-   
-  </script>
+
+
 
   <!-- fin de partie head du calendar -->
 </head>
@@ -74,7 +36,6 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
         <div class="header">
             <ul class="header_list">
                 <li> <a href="LOGIN/login.php"><button type="button" class="btn btn-outline-light">Espace Administrateur</button> </a> </li>
-                
             </ul>
         </div>
         <div class="calendarImg">
@@ -101,6 +62,8 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
                     <ul class="timeline">
                     <?php
               $reponse = $bdd->query('SELECT * FROM calendar ORDER BY date_db DESC LIMIT 3');
+              $sal = $bdd->query('SELECT * FROM salle');
+              $reqsal=$sal->fetch();
 
               while ($donnees = $reponse->fetch())
               { 
@@ -108,6 +71,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
                         <li>
                             <a target="_blank" href="#"><strong> <?php echo  $donnees['intitulé']?> </strong></a>
                             <a href="#" class="float-right">  De <?php echo $donnees['date_db']; ?> à <?php echo $donnees['date_fn']; ?></a>
+                            <p><strong>Salle : </strong><?php echo $reqsal['nom_salle']; ?></p>
                             <p><?php echo $donnees['commentaire']; ?></p>
                         </li>
                         <?php } ?>
@@ -115,6 +79,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
                 </div>
             </div>
         </div>
+
 
     <!-- div du view calendar -->
     <center>
@@ -124,6 +89,8 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
         <br />
         <div id="calendar"></div>
     </div></center>
+    
+
 
 
 
@@ -143,5 +110,82 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
 	} /* speed */ );
 });
     </script>
+
+            <!-- Modal -->
+  <div class="modal fade" id="details" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <h5> Intitulé : </h5>
+            <p id="intitule"></p>
+            <!-- <hr> -->
+            <!-- <h5> Thème : </h5>
+            <p id="theme"> </p> -->
+            <hr>
+            <h5> Date début : </h5>
+            <p id="datedeb"> </p>
+            <hr>
+            <h5> Date fin : </h5>
+            <p id="datefn"> </p>
+            <hr>
+            <!-- <h5> Salle : </h5>
+            <p id="salle"> </p>
+            <hr> -->
+            <!-- <h5> Déscription : </h5>
+            <p id="comment"> </p> -->
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- fin modal -->
+  <script>
+   
+   $(document).ready(function() {
+    var calendar = $('#calendar').fullCalendar({
+     editable: false,
+     header:{
+      left:'prev,next today',
+      center:'title',
+      right:'month,agendaWeek,agendaDay'
+     },
+     <?php $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''); ?>
+     events:[ <?php $reponse = $bdd->query('SELECT * FROM calendar'); 
+                   while ($donnees = $reponse->fetch())
+     { ?>
+         {
+             'title':"<?php echo  $donnees['intitulé'];?>",
+             'start':'<?php echo $donnees['date_db']; ?>',
+             'end':'<?php echo $donnees['date_fn']; ?>',
+             
+             
+         },
+     
+ <?php } ?>],
+     eventClick:function(events)
+     {
+         console.log(events)
+         document.getElementById('intitule').innerHTML=events.title; 
+         document.getElementById('datedeb').innerHTML=events.end;
+         document.getElementById('datefn').innerHTML=events.start;
+        //  document.getElementById('comment').innerHTML=events.extendedProps.commentaire;
+         $('#details').modal('toggle');
+     },
+    });
+   });
+    
+   </script>
+  
 </body>
 </html>
