56e426f6671fc0d8bba03f91e09c79259689e258
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-01-31 00:05:39 +0100

    Update home2.php

--- home2.php
+++ home2.php
@@ -17,7 +17,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', ''
     
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
 
-   <link rel="stylesheet" href="Style2.css">
+   <link rel="stylesheet" href="stylehome.css">
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
